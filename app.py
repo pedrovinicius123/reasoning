423a1ab8d81c45b2851172e608080d2a5fe07f45
Pedro Vinícius de Almeida Silva Xavier <120400955+pedrovinicius123@users.noreply.github.com>
2026-04-05 21:56:03 -0300

    Add testing block for NetworkxParserManger
    Added a testing block to dump best results using NetworkxParserManger.

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from app.utils.networkx_parser import NetworkxParserManger
 
 app = create_app()
 if __name__ == "__main__":
+    # TESTING!
     with app.app_context():
         manager = NetworkxParserManger()
         manager.dump_best()
